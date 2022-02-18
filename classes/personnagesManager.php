5b5dbbd28347d8ca4ce0a1d2511df252b5f0bbbf
AxelCF <axelmaurice1997@gmail.com>
2022-02-18 11:33:17 +0100

    maj

--- classes/personnagesManager.php
+++ classes/personnagesManager.php
@@ -46,23 +46,3 @@ Class PersonnagesManager{
     }
 
 } ?>
-
-<!--
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-<form action="../process/envoi_perso.php" method="POST" > 
-    <label for="personnage">Pseudo</label>
-        <input type="text" name="personnage" placeholder="pseudo">
-</form>
-
-</body>
-</html>
---> 
